eb63c08fa3526f99d2bb24451a4500a3340d6bc7
Scharly <scharly.opitz@gmail.com>
2025-01-28 11:28:15 +0100

    Ajout Lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import VideoScroll from "./components/VideoScroll";
+import Lenis from "lenis";
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main>
       <VideoScroll />
